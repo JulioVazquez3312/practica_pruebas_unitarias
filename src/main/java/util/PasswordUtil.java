7d88ffa3d8cd1ac52b133d8140ee5ba8bbc01e68
Julio3312 <juliovazquezm01@gmail.com>
2022-10-04 12:32:53 -0500

    ya corregi la logica para las contraseñas

--- src/main/java/util/PasswordUtil.java
+++ src/main/java/util/PasswordUtil.java
@@ -2,10 +2,13 @@ package util;
 
 public class PasswordUtil {
     public enum SecurityLevel {
-        WEAK, MEDIUM, STRONG
+        NULL,WEAK, MEDIUM, STRONG
     }
 
-    public static SecurityLevel assessPassword(String password) {
+    public SecurityLevel assessPassword(String password) {
+
+        if (password.isEmpty()||password.isBlank())
+            return SecurityLevel.NULL;
 
         if (password.length() < 8) {
             return SecurityLevel.WEAK;
@@ -18,8 +21,18 @@ public class PasswordUtil {
         if (password.matches("[a-zA-Z0-9]+")) {
             return SecurityLevel.MEDIUM;
         }
+        //para ser fuerte necesita tener
+        //    Minimo 8 caracteres
+        //    Maximo 15
+        //    Al menos una letra mayúscula
+        //    Al menos una letra minucula
+        //    Al menos un dígito
+        //    No espacios en blanco
+        //    Al menos 1 caracter especial
+        if (password.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[$@$!%*?&])[A-Za-z\\d$@$!%*?&]{8,15}"))
+            return SecurityLevel.STRONG;
 
-        return SecurityLevel.STRONG;
+        return SecurityLevel.WEAK;
 
     }
 }
--- src/test/java/util/PasswordUtilTest.java
+++ src/test/java/util/PasswordUtilTest.java
@@ -1,24 +1,34 @@
 package util;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 import static util.PasswordUtil.SecurityLevel.*;
 
 public class PasswordUtilTest {
+    private PasswordUtil password;
+    @Before
+    public void setup(){
+        password = new PasswordUtil();
+    }
 
     @Test
     public void weak_when_has_only_letters() {
-        assertEquals(WEAK, PasswordUtil.assessPassword("13123s"));
+        assertEquals(WEAK, password.assessPassword("13123s"));
+    }
+    @Test
+    public void null_when_has_no_letters() {
+        assertEquals(WEAK, password.assessPassword("     #!!###############"));
     }
 
     @Test
     public void medium_when_has_letters_and_numbers() {
-        assertEquals(MEDIUM, PasswordUtil.assessPassword("abcde12345"));
+        assertEquals(MEDIUM, password.assessPassword("abcde12345"));
     }
 
     @Test
     public void strong_when_has_letters_numbers_and_symbols() {
-        assertEquals(STRONG, PasswordUtil.assessPassword("abcd123!"));
+        assertEquals(STRONG, password.assessPassword("Adasd@2123"));
     }
 }
