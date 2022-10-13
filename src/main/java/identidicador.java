c9c3e50ea64e62482ebef1db2c160c186cd762bf
Julio3312 <juliovazquezm01@gmail.com>
2022-10-13 11:49:55 -0500

    modificacon de varios metodos

--- src/main/java/identidicador.java
+++ src/main/java/identidicador.java
@@ -6,15 +6,30 @@ public class identidicador {
             'K','L','M','N','Ñ','O','P','Q','R','S','T','U',
             'V','W','X','Y','Z'};
     char sim [] = {'_', '$'};
-    char num [] = {0,1,2,3,4,5,6,7,8,9};
-    String entrada="Av1_$";
+    char num [] = {'0','1','2','3','4','5','6','7','8','9'};
+    String entrada="_$aaA_13()112S_Av1_$";
+
+    public identidicador() {
+        revicionNum();
+        verificadorEspacios();
+        letraIn();
+    }
 
     public void verificadorEspacios(){
+        int num=0;
         for (int i = 0; i < entrada.length() ; i++) {
             if (entrada.charAt(i) == ' ' ) {
-                System.out.println("No se puede contener espacios en medio de las palabras");
+                num++;
             }
         }
+        if (num<0)
+            System.out.println("No se puede contener espacios en medio de las letras");
+        else
+            System.out.println("Esta bien sin espacios");
+    }
+
+    public void verificadorLetras(){
+
     }
 
     public int cicloLetra(){
@@ -41,9 +56,21 @@ public class identidicador {
         if (entrada.charAt(0) != '0'& entrada.charAt(0) !='1'
                 & entrada.charAt(0) !='2'& entrada.charAt(0) !='3'& entrada.charAt(0) !='4'
                 & entrada.charAt(0) !='5' & entrada.charAt(0) !='6'& entrada.charAt(0) !='7'
-                & entrada.charAt(0) !='8'& entrada.charAt(0) !='9') {
-            System.out.println("No se puede iniciar con numeros");
+                & entrada.charAt(0) !='8'& entrada.charAt(0) !='9' ) {
+            System.out.println("Esta todo bien sin numeros");
+        } else if (entrada.charAt(0) == '$' & entrada.charAt(0) == '_') {
+            System.out.println("Esta todo bien sin numeros");
+        } else
+           System.out.println("No se puede iniciar con numeros solo con L");
+    }
+
+    public void letraIn(){
+        if (entrada.charAt(0) =='l' || entrada.charAt(0) =='L'|| entrada
+                .charAt(0) == '$' || entrada.charAt(0) == '_') {
+            System.out.println("Inicia correcctamente");
         }
+        else
+            System.out.println("La cadena no puede iniciar con caracteres que no sean l,$,_");
     }
 
     public void revicion(){
@@ -51,5 +78,9 @@ public class identidicador {
                 entrada.charAt(0) ==sim[0]|| entrada.charAt(0) ==num[0]) {
 
         }
+    }
+
+    public static void main(String[] args) {
+        new identidicador();
     }
 }
